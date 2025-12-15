4442b64fed8351203de827854317331ae795262e
Sandeep Singh <166278023+sandeepsingh505@users.noreply.github.com>
2025-12-15 22:37:17 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (46.37%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,16 +1,26 @@
 class Solution {
-    public int helper(int n ,int[]arr){
-        if(n==0 || n==1) return n;
-        if(arr[n]!= -1){
-            return arr[n];
-        }
-       int ans = helper(n-1,arr) + helper(n-2,arr);
-       return ans;
-    }
+// approach  : 1 memoization
+    // public int helper(int n ,int[]arr){
+    //     if(n==0 || n==1) return 1;
+    //     if(arr[n]!= 0){
+    //         return arr[n];
+    //     }
+    //    int ans = helper(n-1,arr) + helper(n-2,arr);
+    //    return ans;
+    // }
     public int fib(int n) {
-        int[] dp = new int[n+1];
-         Arrays.fill(dp,-1);
-         return helper(n,dp);
+        // Tabulation : 
+                if(n<=1) return n;
+
+        int[] dp = new int[3];
+        
+        dp[0] = 0;dp[1] = 1;
+        for(int i = 2;i<=n;i++){
+            dp[2] = dp[0] + dp[1];
+            dp[0] = dp[1];
+            dp[1] = dp[2];
+        }
+         return dp[2];
         
     }
 }
