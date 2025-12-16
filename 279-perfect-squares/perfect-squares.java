586795146a8fe3585268f5cf0541c3cdde5f24ec
Sandeep Singh <166278023+sandeepsingh505@users.noreply.github.com>
2025-12-16 23:44:38 +0530

    Time: 30 ms (72.81%) | Memory: 44.5 MB (51.18%) - LeetSync

--- 279-perfect-squares/perfect-squares.java
+++ 279-perfect-squares/perfect-squares.java
@@ -4,21 +4,31 @@ class Solution {
        return (sq * sq == n);
         
     }
-    public int helper(int n ,int[]dp){
-        if(isperfect(n)) return 1;
-        if(dp[n]!=-1) return dp[n];
-        int minsum = Integer.MAX_VALUE;
-        for(int i =1;i*i<=n;i++){
-            int count = helper(i*i,dp) + helper(n-i*i,dp);
-            minsum = Math.min(minsum,count);
-        }
-        return dp[n] = minsum;
-    }
+    // Approach : 1 Recursion + Memoization
+
+    // public int helper(int n ,int[]dp){
+    //     if(isperfect(n)) return 1;
+    //     if(dp[n]!=-1) return dp[n];
+    //     int minsum = Integer.MAX_VALUE;
+    //     for(int i =1;i*i<=n;i++){
+    //         int count = helper(i*i,dp) + helper(n-i*i,dp);
+    //         minsum = Math.min(minsum,count);
+    //     }
+    //     return dp[n] = minsum;
+    // }
     public int numSquares(int n) {
         int dp[] = new int[n+1];
-        Arrays.fill(dp,-1);
-        return helper(n,dp);
-        
+        dp[0] = 0;
+        for(int i= 1;i<=n;i++){
+             dp[i] =  Integer.MAX_VALUE;
+             for(int j = 1;j*j<=i;j++){
+                 dp[i] = Math.min(dp[i],1  + dp[i-j*j]);
+                
+            }
+           
+
+        }
+         return dp[n];
         
     }
 }
