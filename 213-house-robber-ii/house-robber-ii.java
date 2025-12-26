c72c2f60cd53a8417a91a8c19b1d17d93086a08e
Sandeep Singh <166278023+sandeepsingh505@users.noreply.github.com>
2025-12-26 21:48:45 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (32.24%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,25 +1,21 @@
 class Solution {
-    public int helper(int[]nums,int idx,int lastidx,int []dp){
-        int n = nums.length;
-        if(idx >lastidx) return 0;
-        if(dp[idx]!=-1){
-            return dp[idx];
-        }
-        int pick = nums[idx] + helper(nums,idx+2,lastidx,dp);
-        int notpick = helper(nums,idx+1,lastidx,dp);
-        dp[idx] = Math.max(pick,notpick);
-        return dp[idx];
+    public int helper(int start,int end, int[]nums,int[]dp){
+        if(start>end) return 0;
+        if(dp[start]!=-1) return dp[start];
+        int take = nums[start] + helper(start+2,end,nums,dp);
+        int skip = helper(start+1,end,nums,dp);
+        return dp[start]=Math.max(take,skip);
     }
     public int rob(int[] nums) {
         int n = nums.length;
         if(n==1) return nums[0];
-        int dp1[] = new int[n];
-        int dp2[] = new int[n];
+        int[]dp1 = new int[n];
+        int[]dp2 = new int[n];
         Arrays.fill(dp1,-1);
         Arrays.fill(dp2,-1);
-        int case1 = helper(nums,0,n-2,dp1);
-        int case2 = helper(nums,1,n-1,dp2);
-        return Math.max(case1,case2);
+       int case1 = helper(0,n-2,nums,dp1);
+       int case2 = helper(1,n-1,nums,dp2);
+       return Math.max(case1,case2);
         
     }
 }
