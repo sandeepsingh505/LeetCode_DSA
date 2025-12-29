a06f8c8c097555b2c886ec25c7b6374f5be607e3
Sandeep Singh <166278023+sandeepsingh505@users.noreply.github.com>
2025-12-30 00:52:11 +0530

    Time: 23 ms (42.64%) | Memory: 54.2 MB (16.38%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -13,7 +13,16 @@ class Solution {
                     dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
+        }StringBuilder ans = new StringBuilder("");
+        int i = m,j=n;
+        while(i>0&&j>0){
+            if(a.charAt(i-1)==b.charAt(j-1)){ ans.append(a.charAt(i-1));
+            i--;j--;}
+            else if(dp[i-1][j]>dp[i][j-1]){ i--;}
+            else{ i--;j--;}
         }
+        ans.reverse();
+        System.out.println(ans);
         return dp[m][n];
     }
 }
