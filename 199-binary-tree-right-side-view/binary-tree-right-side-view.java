f489a1d7d5cfb4f75d988832315c1d7209e98302
Sandeep Singh <166278023+sandeepsingh505@users.noreply.github.com>
2026-01-04 19:12:56 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (48.59%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -14,21 +14,36 @@
  * }
  */
 class Solution {
+    // public int levels(TreeNode root){
+    //     if(root==null) return 0;
+    //     return 1 + Math.max(levels(root.left),levels(root.right));
+    // }
+    public void dfs(TreeNode root,int level,List<Integer> ans){
+        if(root==null) return ;
+        if(level==ans.size()) ans.add(root.val);
+        dfs(root.right,level+1,ans);
+        dfs(root.left,level+1,ans);
+
+    }
     public List<Integer> rightSideView(TreeNode root) {
-        Queue<TreeNode> q = new LinkedList<>();
+        // APporach : 1 BFS
+
+        // Queue<TreeNode> q = new LinkedList<>();
         List<Integer> ans = new ArrayList<>();
-        if(root!=null)q.add(root);
-        while(q.size()>0){
-            int size1 = q.size();      // yeh humme batayega ki curr level par kitne nodes hai
-            for(int i = 0;i<size1;i++){
-                TreeNode temp = q.remove();
-                if(i==size1-1){ ans.add(temp.val);}
-                if(temp.left!=null) q.add(temp.left);
-                if(temp.right!=null) q.add(temp.right);
+        // if(root!=null)q.add(root);
+        // while(q.size()>0){
+        //     int size1 = q.size();      // yeh humme batayega ki curr level par kitne nodes hai
+        //     for(int i = 0;i<size1;i++){
+        //         TreeNode temp = q.remove();
+        //         if(i==size1-1){ ans.add(temp.val);}
+        //         if(temp.left!=null) q.add(temp.left);
+        //         if(temp.right!=null) q.add(temp.right);
 
 
-            }
-        }
-        return ans;
+        //     }
+        // }
+        // int lvl = levels(root);
+        dfs(root,0,ans);
+       return ans;
     }
 }
