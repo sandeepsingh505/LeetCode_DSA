1cf9b872a1de732d6edc4d534802bf5610f3188d
Sandeep Singh <166278023+sandeepsingh505@users.noreply.github.com>
2026-01-08 20:16:46 +0530

    Time: 2 ms (13.46%) | Memory: 45.5 MB (16.46%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -14,40 +14,19 @@
  * }
  */
 class Solution {
-    static boolean flag;
-    public static long min(TreeNode root){
-    if(root==null) return Long.MAX_VALUE;
-    Long leftmin = min(root.left);
-    Long rightmin = min(root.right);
-    if(rightmin<=root.val) flag = false;
-    return Math.min(root.val,Math.min(leftmin,rightmin));
-   
-}
-public static long max(TreeNode root){
-    if(root==null) return Long.MIN_VALUE;
-    Long leftmax = max(root.left);
-    if(leftmax>=root.val) flag = false;
-    Long rightmax = max(root.right);
-    return Math.max(root.val,Math.max(leftmax,rightmax));
-    
-}
-    // method 1 : inorder se 
-    // public void inorder(TreeNode root,List<Integer> ans){
-    //     if(root==null) return ;
-    //     inorder(root.left,ans);
-    //     ans.add(root.val);
-    //     inorder(root.right,ans);
-    // }
-
+    public void inorder(TreeNode root,List<Integer> ans){
+        if(root==null) return ;
+        inorder(root.left,ans);
+        ans.add(root.val);
+        inorder(root.right,ans);
+    }
     public boolean isValidBST(TreeNode root) {
-        if(root==null) return true;
-    //    List<Integer> ans = new ArrayList<>();
-    //    inorder(root,ans);
-    //    for(int i = 1;i<ans.size();i++){
-    //     if(ans.get(i)<= ans.get(i-1)) return false;
-    flag = true;
-    max(root);
-    min(root);
-    return flag;
+        List<Integer> ans = new ArrayList<>();
+        inorder(root,ans);
+        for(int i = 1;i<ans.size();i++){
+            if(ans.get(i)<=ans.get(i-1)) return false;
+        }
+        return true;
+        
     }
 }
