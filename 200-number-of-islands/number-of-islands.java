e2e47b58051395738a9283c68c63e04933e088ea
Sandeep Singh <166278023+sandeepsingh505@users.noreply.github.com>
2026-01-10 17:14:34 +0530

    Time: 3 ms (87.20%) | Memory: 52 MB (73.54%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -7,59 +7,23 @@ class Solution {
         for(int row = 0;row<m;row++){
             for(int col=0;col<n;col++){
                 if(visited[row][col]==false && grid[row][col]=='1'){
-                    bfs(row,col,grid,visited);
+                    dfs(row,col,grid,visited);
                     count++;
                 }
             }
         }
         return count;
     }
-    class Pair{
-        int row;
-        int col;
-        Pair(int row,int col){
-            this.row = row;
-            this.col = col;
-        }
+    public void dfs(int i ,int j ,char[][]grid,boolean[][]visited){
+        int m = grid.length;
+        int n = grid[0].length;
+        visited[i][j] = true;
+        if(i-1>=0 && grid[i-1][j]=='1'&&!visited[i-1][j]) dfs(i-1,j,grid,visited);
+        if(i+1<m && grid[i+1][j]=='1'&&!visited[i+1][j]) dfs(i+1,j,grid,visited);
+        if(j-1>=0 && grid[i][j-1]=='1'&&!visited[i][j-1]) dfs(i,j-1,grid,visited);
+        if(j+1<n && grid[i][j+1]=='1'&&!visited[i][j+1]) dfs(i,j+1,grid,visited);
+
+
     }
-        public void bfs(int row,int col,char[][]grid,boolean[][]visited){
-            int m = grid.length;
-            int n = grid[0].length;
-            Queue<Pair> q = new LinkedList<>();
-            q.add(new Pair(row,col));
-            visited[row][col] = true;
-            while(q.size()>0){
-                Pair top = q.remove();
-                int a = top.row , b = top.col;
-                // top =  row > 0
-                if(a>0){
-                if(visited[a-1][b]==false && grid[a-1][b]=='1' ){
-                    q.add(new Pair(a-1,b));
-                    visited[a-1][b] = true;
-                    
-                }
-                }
-                // bottom  row+1 < m
-                if(a+1<m){
-                    if(visited[a+1][b]==false && grid[a+1][b]=='1'){
-                        q.add(new Pair(a+1,b));
-                        visited[a+1][b] = true;
-                    }
-                }
-                  // left  b>0
-                if(b>0){
-                    if(visited[a][b-1]==false && grid[a][b-1]=='1'){
-                        q.add(new Pair(a,b-1));
-                        visited[a][b-1] = true;
-                    }
-                }
-                  // right b<n
-                if(b+1<n){
-                    if(visited[a][b+1]==false && grid[a][b+1]=='1'){
-                        q.add(new Pair(a,b+1));
-                        visited[a][b+1] = true;
-                    }
-                }
-            }
-        }
+    
     }
