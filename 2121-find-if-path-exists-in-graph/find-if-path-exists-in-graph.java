a6e40473bca91217f6817034303840da3ed3c811
Sandeep Singh <166278023+sandeepsingh505@users.noreply.github.com>
2026-02-09 13:59:23 +0530

    Time: 112 ms (58.22%) | Memory: 286.7 MB (31.52%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,27 +1,31 @@
 class Solution {
-    public boolean validPath(int n, int[][] edges, int start, int end) {
-    
-    if(start==end) return true;
-    List<List<Integer>> adj = new ArrayList<>();
-    for(int i = 0;i<n;i++){
-        adj.add(new ArrayList<>());
-    }
-    for(int i = 0;i<edges.length;i++){
-        int a = edges[i][0], b = edges[i][1];
-        adj.get(a).add(b);
-        adj.get(b).add(a);
-    }
-     boolean visited[] = new boolean[n];
-     visited[start] = true;
-    dfs(start,adj,visited);
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i = 0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i = 0;i<edges.length;i++){
+            int a = edges[i][0],b = edges[i][1];
+            adj.get(a).add(b);
+            adj.get(b).add(a);
+        }
+        boolean[]visited = new boolean[n];
+        bfs(source,adj,visited);
+        return visited[destination];
         
-     return visited[end];
     }
-
-    public void dfs(int start,List<List<Integer>> adj,boolean[]visited){
-       visited[start] = true;
-       for(int ele : adj.get(start)){
-        if(!visited[ele]) dfs(ele,adj,visited);
-       } 
-}
+    public void bfs(int start,ArrayList<ArrayList<Integer>> adj,boolean[]visited){
+        Queue<Integer> q = new LinkedList<>();
+        visited[start] = true;
+        q.add(start);
+        while(q.size()>0){
+            int top = q.remove();
+            for(int ele : adj.get(top)){
+                if(!visited[ele]){
+                    visited[ele] = true;
+                    q.add(ele);
+                }
+            }
+        }
+    }
 }
