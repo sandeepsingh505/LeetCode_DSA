8c7ba8180bb9e6b556ced5e1103eb1b619a46416
Sandeep Singh <166278023+sandeepsingh505@users.noreply.github.com>
2026-02-15 15:52:21 +0530

    Time: 10 ms (84.36%) | Memory: 49.5 MB (46.48%) - LeetSync

---
+++ 744-network-delay-time/network-delay-time.java
@@ -0,0 +1,44 @@
+class Solution {
+    class Pair{
+        int node , time;
+        Pair(int node ,int time){
+            this.node = node;
+            this.time = time;
+        }
+    }
+    public int networkDelayTime(int[][] edges, int n, int k) {
+        ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
+        for(int i = 0;i<=n;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i = 0;i<edges.length;i++){
+            int from = edges[i][0] , to = edges[i][1] , wt = edges[i][2];
+            adj.get(from).add(new Pair(to,wt));
+            
+        }
+        int[] distance = new int[n+1];
+        Arrays.fill(distance,Integer.MAX_VALUE);
+        distance[k]=0;
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)-> Integer.compare(a.time,b.time));
+        pq.add(new Pair(k,0));
+        while(pq.size()>0){
+            Pair p = pq.poll();
+            int node = p.node;
+            int ti = p.time;
+            for(Pair ele : adj.get(node)){
+                int next = ele.node;
+                int nexti = ele.time;
+                if(ti + nexti < distance[next]){
+                    distance[next] = ti + nexti;
+                    pq.add(new Pair(next,distance[next]));
+                }
+            }
+        }
+        int max = 0;
+        for(int i = 1;i<=n;i++){
+            if(distance[i]==Integer.MAX_VALUE) return -1;
+            max = Math.max(max,distance[i]);
+        }
+        return max;
+    }
+}
