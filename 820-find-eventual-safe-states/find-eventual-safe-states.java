c45c9c1ffacde0cebde33b9886b003443eda69dd
Sandeep Singh <166278023+sandeepsingh505@users.noreply.github.com>
2026-02-20 11:15:22 +0530

    Time: 30 ms (17.38%) | Memory: 64.2 MB (18.90%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,35 +1,33 @@
 class Solution {
-    public List<Integer> eventualSafeNodes(int[][] edges) {
-      int V = edges.length;
-         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for(int i =0 ;i<V;i++){
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i = 0;i<graph.length;i++){
             adj.add(new ArrayList<>());
         }
-       for(int i = 0;i<edges.length;i++){
-           for(int ele : edges[i]){
-            adj.get(ele).add(i);
-           }  
-       }
-       int[] indegree = new int[V];
-       for(int i = 0 ;i<V;i++){
-        for(int ele : adj.get(i)){
-            indegree[ele]++;
+        for(int i = 0;i<graph.length;i++){
+            for(int ele : graph[i]){
+                adj.get(ele).add(i);
+            }
         }
-       }
-       Queue<Integer> q = new LinkedList<>();
-       List<Integer>  ans = new ArrayList<>();
-       for(int i = 0;i<V;i++){
-        if(indegree[i]==0) q.add(i);
-       }
-       while(q.size()>0) {
-        int node = q.poll();
-        ans.add(node);
-        for(int neigh: adj.get(node)){
-            indegree[neigh]--;
-            if(indegree[neigh]==0) q.add(neigh);
-        }
-       }     
-    Collections.sort(ans);
-    return ans;    
+            int[]indegree = new int[graph.length];
+            Queue<Integer> q = new LinkedList<>();
+            for(int i = 0;i<graph.length;i++){
+                for(int ele  : adj.get(i)) indegree[ele]++;
+            }
+            for(int i = 0;i<graph.length;i++){
+                if(indegree[i]==0)q.add(i);
+            }
+            ArrayList<Integer> ans = new ArrayList<>();
+            while(q.size()>0){
+                int node = q.poll();
+                ans.add(node);
+                for(int ele : adj.get(node)){
+                    indegree[ele]--;
+                    if(indegree[ele]==0) q.add(ele);
+                }
+            }
+      Collections.sort(ans);
+      return ans;
+        
     }
 }
