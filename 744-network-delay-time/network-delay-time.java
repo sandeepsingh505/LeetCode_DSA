3593a1e74d3eb229ad6ddcc76f7ad979871aae9f
Sandeep Singh <166278023+sandeepsingh505@users.noreply.github.com>
2026-02-24 23:47:56 +0530

    Time: 10 ms (85.33%) | Memory: 49.2 MB (57.14%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,43 +1,224 @@
 class Solution {
-    class Pair{
-        int node , time;
-        Pair(int node ,int time){
+    class Pair {
+        int node, time;
+
+        Pair(int node, int time) {
             this.node = node;
             this.time = time;
         }
     }
+
     public int networkDelayTime(int[][] edges, int n, int k) {
         ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
-        for(int i = 0;i<=n;i++){
+        for (int i = 0; i <= n; i++) {
             adj.add(new ArrayList<>());
         }
-        for(int i = 0;i<edges.length;i++){
-            int from = edges[i][0] , to = edges[i][1] , wt = edges[i][2];
-            adj.get(from).add(new Pair(to,wt));
-            
+        for (int i = 0; i < edges.length; i++) {
+            int from = edges[i][0], to = edges[i][1], wt = edges[i][2];
+            adj.get(from).add(new Pair(to, wt));
+
         }
-        int[] distance = new int[n+1];
-        Arrays.fill(distance,Integer.MAX_VALUE);
-        distance[k]=0;
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)-> Integer.compare(a.time,b.time));
-        pq.add(new Pair(k,0));
-        while(pq.size()>0){
+        int[] distance = new int[n + 1];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        distance[k] = 0;
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> Integer.compare(a.time, b.time));
+        pq.add(new Pair(k, 0));
+        while (pq.size() > 0) {
             Pair p = pq.poll();
             int node = p.node;
-            int ti = p.time;
-            for(Pair ele : adj.get(node)){
+            for (Pair ele : adj.get(node)) {
                 int next = ele.node;
                 int nexti = ele.time;
-                if(ti + nexti < distance[next]){
-                    distance[next] = ti + nexti;
-                    pq.add(new Pair(next,distance[next]));
+                if (distance[node] + nexti < distance[next]) {
+                    distance[next] = distance[node] + nexti;
+                    pq.add(new Pair(next, distance[next]));
                 }
             }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
         int min = 0;
-        for(int i = 1;i<=n;i++){
-            if(distance[i]==Integer.MAX_VALUE) return -1;
-            min = Math.max(min,distance[i]);
+        for (int i = 1; i <= n; i++) {
+            if (distance[i] == Integer.MAX_VALUE)
+                return -1;
+            min = Math.max(min, distance[i]);
         }
         return min;
     }
