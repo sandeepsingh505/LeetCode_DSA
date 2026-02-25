bbbfdef5ae6efc3873d0692c920de8ce1cbce20c
Sandeep Singh <166278023+sandeepsingh505@users.noreply.github.com>
2026-02-26 05:19:27 +0530

    Time: 26 ms (86.44%) | Memory: 66.9 MB (87.52%) - LeetSync

---
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.java
@@ -0,0 +1,39 @@
+class Solution {
+    class Pair {
+        int node;
+        double prob;
+        Pair(int n, double p) {
+            node = n;
+            prob = p;
+        }
+    }
+    public double maxProbability(int n, int[][] edges, double[] succProb,int start, int end) {
+        ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++)
+            adj.add(new ArrayList<>());
+        for (int i = 0; i < edges.length; i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            double p = succProb[i];
+            adj.get(u).add(new Pair(v, p));
+            adj.get(v).add(new Pair(u, p));
+        }
+        double[] prob = new double[n];
+        prob[start] = 1.0;
+        PriorityQueue<Pair> pq =new PriorityQueue<>((a, b) ->Double.compare(b.prob, a.prob)); 
+        pq.add(new Pair(start, 1.0));
+        while (!pq.isEmpty()) {
+            Pair curr = pq.poll();
+        if (curr.node == end)
+                return curr.prob;
+for (Pair neighbor : adj.get(curr.node)) {
+double newProb = curr.prob * neighbor.prob;
+    if (newProb > prob[neighbor.node]) {
+        prob[neighbor.node] = newProb;
+            pq.add(new Pair(neighbor.node, newProb));
+    }
+}
+}
+        return 0.0;
+    }
+}
