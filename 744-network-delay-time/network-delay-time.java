618211c69ac34e4b105457e420e890c3c8f975ea
Sandeep Singh <166278023+sandeepsingh505@users.noreply.github.com>
2026-02-27 10:38:59 +0530

    Time: 13 ms (50.97%) | Memory: 48.6 MB (93.82%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,225 +1,33 @@
 class Solution {
-    class Pair {
-        int node, time;
-
-        Pair(int node, int time) {
-            this.node = node;
-            this.time = time;
-        }
-    }
-
     public int networkDelayTime(int[][] edges, int n, int k) {
-        ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
-            adj.add(new ArrayList<>());
-        }
-        for (int i = 0; i < edges.length; i++) {
-            int from = edges[i][0], to = edges[i][1], wt = edges[i][2];
-            adj.get(from).add(new Pair(to, wt));
-
-        }
-        int[] distance = new int[n + 1];
-        Arrays.fill(distance, Integer.MAX_VALUE);
-        distance[k] = 0;
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> Integer.compare(a.time, b.time));
-        pq.add(new Pair(k, 0));
-        while (pq.size() > 0) {
-            Pair p = pq.poll();
-            int node = p.node;
-            for (Pair ele : adj.get(node)) {
-                int next = ele.node;
-                int nexti = ele.time;
-                if (distance[node] + nexti < distance[next]) {
-                    distance[next] = distance[node] + nexti;
-                    pq.add(new Pair(next, distance[next]));
-                }
+        int[]dist = new int[n+1];
+        Arrays.fill(dist,100000000);
+        dist[k] = 0;
+        for(int i = 1;i<=n-1;i++){
+            for(int j=0;j<edges.length;j++){
+               int u = edges[j][0];
+               int v = edges[j][1];
+               int wt = edges[j][2];
+               if(dist[u]!=100000000 && dist[u] + wt < dist[v]){
+                   dist[v] = dist[u] + wt;
+                   }
             }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         }
-        int min = 0;
-        for (int i = 1; i <= n; i++) {
-            if (distance[i] == Integer.MAX_VALUE)
-                return -1;
-            min = Math.max(min, distance[i]);
-        }
-        return min;
+        for(int j=0;j<edges.length;j++){
+               int u = edges[j][0];
+               int v = edges[j][1];
+               int wt = edges[j][2];
+               if(dist[u]!=100000000 && dist[u] + wt < dist[v]){
+                   return -1;
+                   }
+                   
     }
+ int minitime = Integer.MIN_VALUE;
+   for(int i = 1;i<=n;i++){
+    if(dist[i]==100000000) return -1;
+   
+    minitime = Math.max(minitime , dist[i]);
+   }
+   return minitime;
+}
 }
