ffb3ea200040a03342da56ac871147b58219337f
Sandeep Singh <166278023+sandeepsingh505@users.noreply.github.com>
2026-03-17 07:40:40 +0530

    Time: 10 ms (20.07%) | Memory: 51.1 MB (5.78%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.java
+++ 303-range-sum-query-immutable/range-sum-query-immutable.java
@@ -1,14 +1,34 @@
 class NumArray {
-    int []arr;
+   int[]seg ;
+   int n;
     public NumArray(int[] nums) {
-        arr = nums;
-         for(int i = 1;i<arr.length;i++){
-            arr[i] = arr[i-1] + arr[i];
-        }
+        n = nums.length;
+        seg = new int[4*n];
+        build(0,0,n-1,seg,nums);
     }
-    public int sumRange(int left, int right) {
-        if(left == 0) return arr[right];
-       else  return arr[right]-arr[left-1];
+    public void build(int i,int start,int end,int[]seg,int[]nums){
+        if(start==end){
+            seg[i] = nums[start];
+            return;
+        }
+        int mid = start +(end-start)/2;
+        build(2*i+1,start,mid,seg,nums);
+        build(2*i+2,mid+1,end,seg,nums);
+        seg[i] = seg[2*i+1] + seg[2*i+2];
+    }
+    public int query(int i,int start,int end,int l,int r,int[]seg){
+        if(end<l || r<start){
+            return 0;
+        }
+        if(l<=start && end <=r){
+         return   seg[i];
+        }
+        int mid = start+(end-start)/2;
+        return query(2*i+1,start,mid,l,r,seg) + query(2*i+2,mid+1,end,l,r,seg);
+    }
+    public int sumRange(int left, int right){
+        return query(0,0,n-1,left,right,seg);
+        
     }
 }
 
