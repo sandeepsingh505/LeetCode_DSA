72a1c8dcc5544e813ce462a3d41f2aa8950dda85
Sandeep Singh <166278023+sandeepsingh505@users.noreply.github.com>
2026-04-06 14:32:09 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (90.56%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,28 +1,13 @@
 class Solution {
-    // approach 1 : memoization
-    // public int helper(int[] nums, int idx,int[]dp){
-    //     int n = nums.length;
-    //     if(idx>= n  ) return 0 ;
-    //     if(dp[idx]!=-1){
-    //         return dp[idx];
-    //     }
-    //     int pick = nums[idx] +  helper(nums,idx+2,dp);
-    //     int notpick = helper(nums,idx+1,dp);
-    //     dp[idx] = Math.max(pick,notpick);
-    //     return dp[idx];
-    // }
     public int rob(int[] nums) {
-     // Approach 2 : Tabulation
-        int n = nums.length;
-        int dp [] = new int[n+1];
-        if(n==1) return nums[0];
+        int[]dp = new int[nums.length];
         dp[0] = nums[0];
-        dp[1] = Math.max(nums[0],nums[1]);
-        for(int i = 2;i<n;i++){
-            dp[i] = Math.max(nums[i]+ dp[i-2],dp[i-1]);
+      if(nums.length==1) return  dp[0] ;
+         dp[1] = Math.max(dp[0],nums[1]);
+        
+        for(int i = 2;i<nums.length;i++){
+            dp[i] = Math.max(nums[i] +dp[i-2],dp[i-1]);
         }
-        return dp[n-1];
-        
-        
+        return dp[nums.length-1];
     }
 }
