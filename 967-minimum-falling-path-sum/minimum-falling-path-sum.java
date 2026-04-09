45496730926d283aa094a562a8b95cb2905fd07d
Sandeep Singh <166278023+sandeepsingh505@users.noreply.github.com>
2026-04-09 16:44:47 +0530

    Time: 1 ms (99.85%) | Memory: 47.2 MB (71.39%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -2,36 +2,26 @@ class Solution {
     public int minpath(int row , int col ,int[][] mat, int[][] dp){
         int m = mat.length;
         int n = mat[0].length;
-
-        // FIX HERE
         if (col < 0 || col >= n)
             return Integer.MAX_VALUE;
-
         if (row == m - 1)
             return mat[row][col];
-
         if (dp[row][col] != Integer.MAX_VALUE)
             return dp[row][col];
-
         int diagLeft = minpath(row + 1, col - 1, mat, dp);
         int down = minpath(row + 1, col, mat, dp);
         int diagRight = minpath(row + 1, col + 1, mat, dp);
-
         return dp[row][col] =
             mat[row][col] + Math.min(diagLeft, Math.min(down, diagRight));
     }
-
     public int minFallingPathSum(int[][] mat) {
         int[][] dp = new int[mat.length][mat[0].length];
         for (int[] row : dp)
             Arrays.fill(row,Integer.MAX_VALUE);
-
         int ans = Integer.MAX_VALUE;
-
         for (int j = 0; j < mat[0].length; j++) {
             ans = Math.min(ans, minpath(0, j, mat, dp));
         }
-
         return ans;
     }
 }
