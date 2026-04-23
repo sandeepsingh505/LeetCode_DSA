2898fa6f930c55774eba47f179faceb85d6e2440
Sandeep Singh <166278023+sandeepsingh505@users.noreply.github.com>
2026-04-23 23:06:45 +0530

    Time: 43 ms (44.41%) | Memory: 46.1 MB (70.36%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -4,13 +4,15 @@ class Solution {
         int dp[] = new int[n];
         int max = 0;
         for(int i = 0;i<n;i++){
-            for(int j =0;j<=i-1;j++){
-                if(nums[j]<nums[i]) dp[i] = Math.max(dp[i],dp[j]);
+            for(int j = 0;j<=i-1;j++){
+                if(nums[i]>nums[j]){
+                    dp[i] = Math.max(dp[i],dp[j]);
+                }
+               
             }
-            dp[i]+=1;
-            max = Math.max(max,dp[i]);
+             dp[i] += 1;
+                max = Math.max(max,dp[i]);
         }
         return max;
-        
     }
 }
