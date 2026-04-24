6a53e62556f6d1b85ebc098c9a85deb75fc2d125
Sandeep Singh <166278023+sandeepsingh505@users.noreply.github.com>
2026-04-24 10:51:23 +0530

    Time: 2 ms (90.34%) | Memory: 44.2 MB (42.09%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,17 +1,16 @@
 class Solution {
     class Pair{
-        int row , col;
+        int row,col;
         Pair(int row,int col){
             this.row = row;
             this.col = col;
         }
     }
     public int bfs(int r,int c,int[][]grid){
-        int minutes =0;
-        Queue<Pair> q = new LinkedList<>();
         int n = grid.length;
         int m = grid[0].length;
-        int fresh = 0;
+        Queue<Pair> q = new LinkedList<>();
+        int minute = 0 , fresh = 0;
         for(int i = 0;i<n;i++){
             for(int j = 0;j<m;j++){
                 if(grid[i][j]==2){
@@ -21,37 +20,31 @@ class Solution {
                 }
             }
         }
-        
-        int direction[][] = {{1,0},{-1,0},{0,1},{0,-1}};
-        while(q.size()>0){
+        int [][]direction = {{0,1},{1,0},{-1,0},{0,-1}};
+        while(q.size()>0 && fresh>0){
             int size = q.size();
-            boolean rottanthislevel = false;
+            minute++;
             for(int i = 0;i<size;i++){
-            Pair p = q.poll();
-            for(int[]d:direction){
-                int nr = p.row +d[0];
-                int nc = p.col +d[1];
-                if(nr>=0 && nc>=0 && nr<n && nc<m && grid[nr][nc]==1){
-                    grid[nr][nc] = 2;
-                    fresh--;
-                    q.add(new Pair(nr,nc));
-                    rottanthislevel = true;
+                Pair p = q.poll();
+                boolean rottanlevel = false;
+                for(int[]d : direction){
+                    int nr = p.row + d[0];
+                    int nc = p.col + d[1];
+                    if(nr>=0 && nc>=0 && nr<n && nc<m && grid[nr][nc]==1){
+                        rottanlevel = true;
+                        grid[nr][nc] = 2;
+                        fresh--;
+                        q.add(new Pair(nr,nc));
 
+                    }
                 }
             }
-            }
-           if(rottanthislevel) minutes++;
+            
         }
-         
-         if(fresh==0){
-          return  minutes;
-         }
-         return -1;
+        if(fresh==0) return minute;
+        return -1;
     }
     public int orangesRotting(int[][] grid) {
-        
-      return  bfs(0,0,grid);
-    
-        
+        return bfs(0,0,grid);
     }
 }
