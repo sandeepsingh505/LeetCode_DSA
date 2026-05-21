29d3bc957d282d26429d2c801c024a30380a75a3
Sandeep Singh <166278023+sandeepsingh505@users.noreply.github.com>
2026-05-21 15:52:57 +0530

    Time: 25 ms (26.92%) | Memory: 53.9 MB (50.38%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,17 +1,16 @@
 class Solution {
-    public int solve(String s1, String s2,int m,int n,int[][]dp){
-        if(m==0 || n==0) return 0;
+    public int helper(int m,int n,String s1,String s2,int[][]dp){
+        if(m==0||n==0) return 0;
         if(dp[m][n]!=-1) return dp[m][n];
-        if(s1.charAt(m-1)==s2.charAt(n-1)){
-            return dp[m][n] = 1 + solve(s1,s2,m-1,n-1,dp);
+        if(s1.charAt(m-1) == s2.charAt(n-1)){
+            return dp[m][n] = 1 + helper(m-1,n-1,s1,s2,dp);
         }
-        return dp[m][n] = Math.max(solve(s1,s2,m-1,n,dp),solve(s1,s2,m,n-1,dp));
+           return dp[m][n] = Math.max(helper(m,n-1,s1,s2,dp),helper(m-1,n,s1,s2,dp));
+    
     }
-    public int longestCommonSubsequence(String s1, String s2) {
-         int[][]dp = new int[s1.length()+1][s2.length()+1];
-         for(int[]row : dp) Arrays.fill(row,-1);
-         return solve(s1,s2,s1.length(),s2.length(),dp);
-
-        
+    public int longestCommonSubsequence(String text1, String text2) {
+        int[][] dp = new int[text1.length()+1][text2.length()+1];
+        for(int[]row : dp) Arrays.fill(row,-1);
+        return helper(text1.length(),text2.length(),text1,text2,dp);
     }
 }
