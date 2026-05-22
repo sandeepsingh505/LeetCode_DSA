4e1097c61be4ab73fb0223200348b0f1848235c8
Sandeep Singh <166278023+sandeepsingh505@users.noreply.github.com>
2026-05-22 09:40:58 +0530

    Time: 42 ms (55.49%) | Memory: 63.3 MB (81.13%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
@@ -1,17 +1,18 @@
 class Solution {
-    public int helper(int idx1,int idx2,String a ,StringBuilder b,int[][]dp){
-        if(idx1==a.length()||idx2==b.length()) return 0;
-        if(dp[idx1][idx2]!=-1) return dp[idx1][idx2];
-        if(a.charAt(idx1)==b.charAt(idx2)) return dp[idx1][idx2] = 1+helper(idx1+1,idx2+1,a,b,dp);
-        return dp[idx1][idx2]= Math.max(helper(idx1+1,idx2,a,b,dp),helper(idx1,idx2+1,a,b,dp));
+    public int helper(int m,int n ,String s1,String s2,int[][]dp){
+        if(m==0 || n==0) return 0;
+        if(dp[m][n]!=-1) return dp[m][n];
+        if(s1.charAt(m-1)==s2.charAt(n-1)){
+            return dp[m][n] = 1+helper(m-1,n-1,s1,s2,dp);
+        }
+        return dp[m][n] = Math.max(helper(m-1,n,s1,s2,dp),helper(m,n-1,s1,s2,dp));
     }
     public int longestPalindromeSubseq(String s) {
-        StringBuilder rev = new StringBuilder(s);
-        rev.reverse();
-        int dp[][] = new int[s.length()][rev.length()];
-        for(int row[]:dp) Arrays.fill(row,-1);
-        return helper(0,0,s,rev,dp);
-
-        
+     StringBuilder sb = new StringBuilder(s);
+     sb.reverse();
+     String srev = sb.toString();
+     int[][]dp = new int[s.length()+1][s.length()+1];
+     for(int[]row:dp) Arrays.fill(row,-1);
+     return helper(s.length(),s.length(),s,srev,dp);
     }
 }
