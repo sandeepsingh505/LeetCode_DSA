40dbf7fe8cb025a8f0a09bee515050f4913409c8
Sandeep Singh <166278023+sandeepsingh505@users.noreply.github.com>
2026-06-10 17:43:19 +0530

    Time: 1 ms (61.44%) | Memory: 47.3 MB (36.57%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,21 +1,21 @@
 class Solution {
-    static boolean ans;
+    static boolean ans ;
     public boolean isBipartite(int[][] graph) {
-        ans = true;
-        int n = graph.length;
-        int[]visited = new int[n];
-        Arrays.fill(visited,-1);
-        for(int i = 0;i<n;i++){
-            if(ans == false ) return ans;
-            else if(visited[i]==-1) bfs(i,graph,visited);
-        }
-        return ans;
+     ans = true;
+     int n = graph.length;
+     int[]visited = new int [n];
+     Arrays.fill(visited,-1);
+     for(int i = 0;i<n;i++){
+        if(ans == false ) return ans ;
+        else if(visited[i]==-1) bfs(i,graph,visited);
+     }
+     return ans;
     }
-    public void bfs(int start,int[][]graph, int[]visited){
+    public void bfs(int start, int[][]graph , int[]visited){
         Queue<Integer> q = new LinkedList<>();
-        visited[start] = 0;   // 0- red , 1 - blue;
+        visited[start] = 0;    // 0- red , 1 - blue
         q.add(start);
-        while(q.size()>0) {
+        while(q.size()>0){
             int front = q.poll();
             int color = visited[front];
             for(int ele : graph[front]){
