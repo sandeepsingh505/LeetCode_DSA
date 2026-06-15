0f76ec78d30048c3d5b30372d04cb2b304aa8f98
Sandeep Singh <166278023+sandeepsingh505@users.noreply.github.com>
2026-06-15 11:34:44 +0530

    Time: 5 ms (69.96%) | Memory: 47.2 MB (33.11%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,25 +1,26 @@
 class Solution {
-    public int solve(int i,int j , String s1,String s2,int[][]dp){
-        int  n = s1.length() , m = s2.length();
-        if(i==n){  // mtlb word1 empty ya katam ho gaya hai toh insertion chaiye jitne word2 hai 
-          return  m-j;
-        }
-          if(j==m){  // mtlb word2 empty ya katam ho gaya hai toh deletion chaiye jitne word1 hai 
-          return  n-i;
-        }
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s1.charAt(i)==s2.charAt(j)){// character match ho gaya toh i+1,j+1
-            return dp[i][j] =  solve(i+1,j+1,s1,s2,dp);
-        }else{
-            int insert = 1 + solve(i,j+1,s1,s2,dp);
-            int delete = 1 + solve(i+1,j,s1,s2,dp);
-            int replace = 1 + solve(i+1,j+1,s1,s2,dp);
-            return dp[i][j] =  Math.min(insert,(Math.min(delete,replace)));
-        }
-    }
     public int minDistance(String word1, String word2) {
-        int[][]dp = new int[word1.length()+1][word2.length()+1];
-        for(int[]row : dp) Arrays.fill(row,-1);
-        return solve(0,0,word1,word2,dp);
+        int n = word1.length() , m = word2.length();
+        int[][]dp = new int[n+1][m+1];
+        for(int i = 0;i<=n;i++){
+            dp[i][0] = i;
+        }
+        for(int j = 0;j<=m;j++){
+            dp[0][j] = j;
+        }
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=m;j++){
+              if(word1.charAt(i-1)== word2.charAt(j-1)){
+                dp[i][j] = dp[i-1][j-1];
+              }else{
+                int insert = dp[i][j-1];
+                int delete = dp[i-1][j];
+                int replace = dp[i-1][j-1];
+                dp[i][j] = 1+ Math.min(insert,Math.min(delete,replace));
+              }
+            }
+           
+        }
+         return dp[n][m];
     }
 }
