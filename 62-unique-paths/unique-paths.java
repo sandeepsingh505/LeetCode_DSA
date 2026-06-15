faca269a602754976e7907272b1830d88a2d75d0
Sandeep Singh <166278023+sandeepsingh505@users.noreply.github.com>
2026-06-15 11:50:00 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (95.60%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,18 +1,19 @@
 class Solution {
- public int path(int i,int j,int[][]grid,int m ,int n,int dp[][]){
-    if(i>=m || j>=n) return 0;
-    if(i==m-1 && j==n-1) return 1;
-    if(dp[i][j]!=-1) return dp[i][j];
-    int down = path(i+1,j,grid,m,n,dp);
-    int right = path(i,j+1,grid,m,n,dp);
-    return dp[i][j] = down + right;
-    
- }
+    public int solve(int i ,int j ,int m,int n,int[][]dp){
+        if(i==m-1 && j==n-1){
+            return 1;
+        }
+        if(i>=m || j>=n) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int right = 0, down = 0;
+      if(i<m){   right = solve(i,j+1,m,n,dp);}
+       if(j<n) { down = solve(i+1,j,m,n,dp);}
+        return dp[i][j]=right + down;
+
+    }
     public int uniquePaths(int m, int n) {
-        int grid[][] = new int[m][n];
-        int dp[][] = new int[m][n];
+        int[][]dp = new int[m+1][n+1];
         for(int[]row : dp) Arrays.fill(row,-1);
-        return path(0,0,grid,m,n,dp);
-        
+        return solve(0,0,m,n,dp);
     }
 }
