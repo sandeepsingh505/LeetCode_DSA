b42b69cb59ae71c8027917f246f5cb76072503c4
Sandeep Singh <166278023+sandeepsingh505@users.noreply.github.com>
2026-06-15 12:41:39 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (18.27%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,13 +1,15 @@
 class Solution {
+    public int solve(int i ,int[]arr,int[]dp){
+        if(i>=arr.length) return 0;
+        if(dp[i]!=-1) return dp[i];
+        int pick = arr[i] + solve(i+2,arr,dp);
+        int notpick = solve(i+1,arr,dp);
+        return dp[i] = Math.max(pick,notpick);
+    }
     public int rob(int[] nums) {
-        int[]dp = new int[nums.length];
-        dp[0] = nums[0];
-      if(nums.length==1) return  dp[0] ;
-         dp[1] = Math.max(dp[0],nums[1]);
+        int []dp = new int[nums.length];
+       Arrays.fill(dp,-1);
+        return solve(0,nums,dp);
         
-        for(int i = 2;i<nums.length;i++){
-            dp[i] = Math.max(nums[i] +dp[i-2],dp[i-1]);
-        }
-        return dp[nums.length-1];
     }
 }
