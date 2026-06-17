01b61e095040a6a4ef1197383725b0e1aa012cb1
Sandeep Singh <166278023+sandeepsingh505@users.noreply.github.com>
2026-06-17 17:27:21 +0530

    Time: 2 ms (90.88%) | Memory: 44.3 MB (25.05%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,46 +1,44 @@
 class Solution {
     class Pair{
-        int row , col;
+        int row,col;
         Pair(int row,int col){
             this.row = row;
             this.col = col;
-        
         }
     }
     public int orangesRotting(int[][] grid) {
+        int m = grid.length , n = grid[0].length;
         Queue<Pair> q = new LinkedList<>();
-         int fresh = 0,minutes = 0;
-         int n = grid.length , m = grid[0].length;
-         for(int i = 0;i<n;i++){
-            for(int j = 0;j<m;j++){
+        int fresh = 0,minutes = 0;
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
                 if(grid[i][j]==2){
                     q.add(new Pair(i,j));
                 }else if(grid[i][j]==1){
                     fresh++;
                 }
             }
-         }
-         int[][]directions = {{1,0},{0,1},{-1,0},{0,-1}};
-         while(q.size()>0){
+        }
+        int[][]directions = {{1,0},{0,1},{-1,0},{0,-1}};
+        while(q.size()>0){
+            boolean rottanthislevel = false;
             int size = q.size();
-            boolean rottenthislevel = false;
             for(int i = 0;i<size;i++){
                 Pair p = q.poll();
-                for(int[]d : directions){
+                for(int []d : directions){
                     int nr = p.row + d[0];
                     int nc = p.col + d[1];
-                    if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==1){
+                    if(nr>=0 && nc>=0 && nr<m && nc<n && grid[nr][nc]==1){
                         grid[nr][nc] = 2;
-                        rottenthislevel = true;
                         fresh--;
                         q.add(new Pair(nr,nc));
-
+                        rottanthislevel = true;
                     }
                 }
             }
-            if(rottenthislevel) minutes++;
-         }
-         if(fresh==0) return minutes;
-         else return -1;
+            if(rottanthislevel) minutes++;
+        }
+        return (fresh==0) ? minutes : -1;
+        
     }
 }
