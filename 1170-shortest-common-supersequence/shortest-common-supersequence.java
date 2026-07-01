936cf3a0184f95c7c2cb59191b3fc853f186dd0b
Sandeep Singh <166278023+sandeepsingh505@users.noreply.github.com>
2026-07-01 19:59:43 +0530

    Time: 23 ms (72.21%) | Memory: 54.3 MB (9.31%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.java
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.java
@@ -1,48 +1,38 @@
 class Solution {
-    public String lcs(String a, String b ){
-        int[][] dp= new int[a.length()+1][b.length()+1];
-        for(int i = 1;i<=a.length();i++){
-            for(int j = 1;j<=b.length();j++){
-                if(a.charAt(i-1)==b.charAt(j-1)) dp[i][j] = 1 + dp[i-1][j-1];
-                else dp[i][j] = Math.max(dp[i][j-1],dp[i-1][j]);
+    public String shortestCommonSupersequence(String str1, String str2) {
+        int n = str1.length() , m = str2.length();
+        int[][]dp = new int[n+1][m+1];
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=m;j++){
+                if(str1.charAt(i-1)==str2.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
             }
         }
-        int i = a.length(),j = b.length();
-         StringBuilder sb = new StringBuilder("");
-        while(i> 0 && j>0){
-            if(a.charAt(i-1)==b.charAt(j-1)){
-                 sb.append(a.charAt(i-1));
-                 i--;
-                 j--;
-                 }
-            else if(dp[i][j-1]>dp[i-1][j]) j--;
-            else i--;;
+        StringBuilder result = new StringBuilder();
+        int i = n , j = m;
+        while(i>0 && j>0){
+            if(str1.charAt(i-1)==str2.charAt(j-1)){
+                    result.append(str1.charAt(i-1));
+                    i--;
+                    j--;
+                }else if(dp[i-1][j]> dp[i][j-1]){
+                    result.append(str1.charAt(i-1));
+                    i--;
+                }else{
+                    result.append(str2.charAt(j-1));
+                    j--;
+                }
         }
-        sb.reverse();
-        return sb.toString();
-    
+        while(i>0){
+            result.append(str1.charAt(i-1));
+            i--;
+        }while(j>0){
+            result.append(str2.charAt(j-1));
+            j--;
         }
-       
-    public String shortestCommonSupersequence(String a, String b) {
-        String lc = lcs(a,b);
-        int i = 0,j=0,k=0;
-        String sb = "";
-        while(i<a.length()&& j<b.length()&& k<lc.length()){
-            while(a.charAt(i)!=lc.charAt(k)){
-                sb +=a.charAt(i); i++;
-           
-            }while(b.charAt(j)!=lc.charAt(k)){
-                sb +=b.charAt(j); j++;
-            }
-            sb +=lc.charAt(k);
-            i++;j++;k++;
-        }
-        while(j<b.length()){
-            sb+=b.charAt(j);j++;
-        }while(i<a.length()){
-            sb +=a.charAt(i);i++;
-        }
-        return sb;
-     
+        return result.reverse().toString();
     }
 }
