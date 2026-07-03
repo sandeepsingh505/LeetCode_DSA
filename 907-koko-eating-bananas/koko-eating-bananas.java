5b7b9abf35f0b2ad729c76a39faf4757c8a4f79d
Sandeep Singh <166278023+sandeepsingh505@users.noreply.github.com>
2026-07-03 06:36:25 +0530

    Time: 6 ms (99.51%) | Memory: 47.6 MB (86.45%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,32 +1,27 @@
 class Solution {
-    public int Maxvalue(int[]arr){
-        int max = 0;
-        for(int i = 0;i<arr.length;i++){
-            if(max<arr[i]) max = arr[i];
+    public int minEatingSpeed(int[] piles, int h) {
+        int low = 1; 
+        int high = 0;
+        for(int pile : piles){
+            high = Math.max(high,pile);
         }
-        return max;
-    }
-    public boolean isPossible(int[]arr,int hour,int speed){
-        int time = 0;
-        for(int i = 0;i<arr.length;i++){
-            time += Math.ceil((double)arr[i]/speed);
-        }
-        if(time<=hour) return true;
-        return false;
-    }
-    public int minEatingSpeed(int[] piles, int k) {
-        int start = 1;
-        int end = Maxvalue(piles);
-        int ans = 0;
-        while(start<=end){
-            int mid = (end +start)/2;
-            if(isPossible(piles,k,mid)){
+        int ans = high;
+        while(low <=high){
+            int mid  = low + (high-low)/2;
+            if(isValid(piles,mid,h)){
                 ans = mid;
-                end = mid-1;
+                high = mid-1;
             }else{
-                start = mid+1;
+                low = mid+1;
             }
         }
         return ans;
+    }
+    public boolean isValid(int[]piles,int speed,int h){
+        long hour = 0;
+        for(int pile : piles){
+            hour += (pile+speed-1)/speed;
+        }
+        return (hour<=h);
     }
 }
