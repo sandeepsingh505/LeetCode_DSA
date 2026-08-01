7c4d0e2a2456148db06a4779a54b88e22300f20c
Sandeep Singh <166278023+sandeepsingh505@users.noreply.github.com>
2026-08-01 09:48:41 +0530

    Time: 1 ms (70.42%) | Memory: 43.4 MB (93.88%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -14,36 +14,24 @@
  * }
  */
 class Solution {
-    // public int levels(TreeNode root){
-    //     if(root==null) return 0;
-    //     return 1 + Math.max(levels(root.left),levels(root.right));
-    // }
-    public void dfs(TreeNode root,int level,List<Integer> ans){
-        if(root==null) return ;
-        if(level==ans.size()) ans.add(root.val);
-        dfs(root.right,level+1,ans);
-        dfs(root.left,level+1,ans);
-
-    }
     public List<Integer> rightSideView(TreeNode root) {
-        // APporach : 1 BFS
-
-        // Queue<TreeNode> q = new LinkedList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        if(root!=null) q.offer(root);
         List<Integer> ans = new ArrayList<>();
-        // if(root!=null)q.add(root);
-        // while(q.size()>0){
-        //     int size1 = q.size();      // yeh humme batayega ki curr level par kitne nodes hai
-        //     for(int i = 0;i<size1;i++){
-        //         TreeNode temp = q.remove();
-        //         if(i==size1-1){ ans.add(temp.val);}
-        //         if(temp.left!=null) q.add(temp.left);
-        //         if(temp.right!=null) q.add(temp.right);
+        while(q.size()>0){
+            int size = q.size();
+            for(int i = 0;i<size;i++){
+                 TreeNode node = q.remove();
+                if(i == size-1){                 
+                   ans.add(node.val);}
+                   if(node.left!=null) q.add(node.left);
+                   if(node.right!=null)q.add(node.right);
+                
+               
+            }
 
+        }
+           return ans;
 
-        //     }
-        // }
-        // int lvl = levels(root);
-        dfs(root,0,ans);
-       return ans;
     }
 }
