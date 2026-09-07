eaa0495f50b718adfef3508b045f0ba27046286c
Sandeep Singh <166278023+sandeepsingh505@users.noreply.github.com>
2026-09-07 10:04:07 +0530

    Time: 643 ms (39.86%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-select  employee_id ,department_id From Employee a where primary_flag = 'Y' OR (select count(*) from employee b where a.employee_id = b.employee_id) = 1;
+select  employee_id ,department_id From Employee a where primary_flag = 'Y' OR (select count(department_id) from employee b where a.employee_id = b.employee_id) = 1;
