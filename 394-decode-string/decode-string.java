977848c1be3c093ee29fc61d17e3e3ee9d2aa8cf
Sandeep Singh <166278023+sandeepsingh505@users.noreply.github.com>
2026-09-13 07:37:58 +0530

    Time: 2 ms (34.75%) | Memory: 43.2 MB (9.09%) - LeetSync

--- 394-decode-string/decode-string.java
+++ 394-decode-string/decode-string.java
@@ -1,29 +1,30 @@
 class Solution {
     public String decodeString(String s) {
-        Stack<Integer> stac = new Stack<>();
-        Stack<StringBuilder> stacSB = new Stack<>();
-        StringBuilder str = new StringBuilder();
-        int n = 0;
-        for (char c : s.toCharArray()) {
-            if (Character.isDigit(c)) {
-                n = n * 10 + (c - '0');
-            } else if (c == '[') {
-                stac.push(n);
-                n = 0;
-                stacSB.push(str);
-                str = new StringBuilder();
-            } else if (c == ']') {
-                int k = stac.pop();
-                StringBuilder temp = str;
-                str = stacSB.pop();
-                while (k-- > 0) {
-                    str.append(temp);
+        Stack<Integer> st1 = new Stack<>();
+        Stack<String> st2 = new Stack<>();
+        int currNum = 0;
+        String currString = "";
+        for(char ch : s.toCharArray()){
+            if(Character.isDigit(ch)){
+                currNum = currNum * 10 + ch-'0';
+            }else if(ch=='['){
+                st1.push(currNum);
+                st2.push(currString);
+                currNum = 0;
+                currString = "";
+            }else if(ch==']'){
+                int num = st1.pop();
+                String str = st2.pop();
+                StringBuilder sb = new StringBuilder(str);
+                for(int i = 0;i<num;i++){
+                    sb.append(currString);
                 }
-            } else {
-                str.append(c);
+                currString = sb.toString();
+                
+            }else{
+                currString += ch;
             }
         }
-
-        return str.toString();
+        return currString;
     }
 }
