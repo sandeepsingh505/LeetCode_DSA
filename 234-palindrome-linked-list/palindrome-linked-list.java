05cf904309ff11dbf51d91105a1c09501db029d5
Sandeep Singh <166278023+sandeepsingh505@users.noreply.github.com>
2026-09-26 12:38:41 +0530

    Time: 4 ms (67.55%) | Memory: 94.1 MB (89.46%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -10,21 +10,36 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        // store node  into stack 
-        Stack<Integer> st = new Stack<>();
-        ListNode temp = head;
-        while(temp!=null){
-            st.push(temp.val);
-            temp = temp.next;
+        // find the middle and reverse the second half
+        // if(head==null||head.next==null) return true;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        temp = head;
-        while(temp!=null){
-            if(temp.val != st.pop()){
+        ListNode firsthalf = head;
+        ListNode secondhalf = reverse(slow);
+        ListNode curr = secondhalf;
+        while(curr!=null){
+            if(firsthalf.val!=curr.val){
                 return false;
             }
-        temp = temp.next;
+            firsthalf = firsthalf.next;
+            curr = curr.next;
         }
         return true;
-       
+        
+    }
+    public ListNode reverse(ListNode middle){
+        ListNode prev = null;
+        ListNode curr = middle;
+        while(curr!=null){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
     }
 }
